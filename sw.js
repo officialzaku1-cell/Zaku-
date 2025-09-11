439106f31971c22a481703c397198773fab1e788
officialzaku1-cell <officialzaku1@gmail.com>
2025-09-11 06:00:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,23 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("zaku-cache").then(cache => {
       return cache.addAll([
-        "/off.html"
-      ]);
+        "index.html",   // homepage
+        "z.html",       // checkout page
+        "checkout.html",// optional checkout confirmation
+        "off.html"  // offline fallback
+        "failed.html"
+      "success.html"
+        ]);
     })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
-  if (!navigator.onLine) {
-    event.respondWith(
-      caches.match("/off.html")
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      // If request fails, show offline page
+      return caches.match("off.html");
+    })
+  );
 });
-
-
